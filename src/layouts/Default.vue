6746efd4cdb889bc537c88ecd325230bbb23ae73
Ronnie Villarini <Villarini91@gmail.com>
2020-07-10 15:07:20 -0500

    gridsome reboot

---
+++ src/layouts/Default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="layout">
+        <Nav />
+        <main id="main">
+            <slot />
+        </main>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Default',
+    components: {
+        Nav: () => import('./Nav.vue'),
+    },
+};
+</script>
