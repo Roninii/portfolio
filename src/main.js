645a8f8f782959fe360b2c2f5c48602eb22d8ec8
Ronnie Villarini <>
2020-08-13 11:44:24 -0500

    add defer and async attrs to font awesome

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import 'victormono';
-import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+// import "./assets/prism.css"
 import 'gridsome-plugin-remark-prismjs-all/themes/night-owl.css';
 import '~/assets/style.css';
 
 // plugins
 import VueCompositionAPI from '@vue/composition-api';
 
-export default function(Vue, { router, head, isClient }) {
+export default function(Vue, { router, head, body, isClient }) {
     head.script.push({
         src: 'https://kit.fontawesome.com/963f59e308.js',
         body: false,
+        async: true,
+        defer: true
     });
 
     Vue.use(VueCompositionAPI);
