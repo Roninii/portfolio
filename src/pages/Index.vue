efc34ba07a5a11a8993b802d099cf8f1b05ca2f5
Ronnie Villarini <>
2020-08-24 19:16:19 -0500

    replace face recognition brain with WS website

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,7 +4,8 @@
 
         <section class="contact">
             <h2 class="contact__heading">
-                Let's <span class="highlight--primary">build</span> something
+                Let's
+                <span class="highlight--primary">build</span> something
                 <span class="highlight--primary">together</span>
             </h2>
             <div class="contact__links">
@@ -24,8 +25,9 @@
                     rel="noreferrer"
                     class="contact__link"
                     title="github"
-                    ><i class="fab fa-github contact__icon"></i
-                ></a>
+                >
+                    <i class="fab fa-github contact__icon"></i>
+                </a>
 
                 <a
                     href="https://linkedin.com/in/ronnievillarini"
@@ -33,8 +35,9 @@
                     class="contact__link"
                     title="linkedin"
                     rel="noreferrer"
-                    ><i class="fab fa-linkedin-in contact__icon"></i
-                ></a>
+                >
+                    <i class="fab fa-linkedin-in contact__icon"></i>
+                </a>
 
                 <a
                     href="https://twitter.com/_ronini"
@@ -42,7 +45,8 @@
                     class="contact__link"
                     title="twitter"
                     rel="noreferrer"
-                    ><i class="fab fa-twitter contact__icon"></i>
+                >
+                    <i class="fab fa-twitter contact__icon"></i>
                 </a>
 
                 <a
@@ -51,7 +55,8 @@
                     class="contact__link"
                     title="email"
                     rel="noreferrer"
-                    ><i class="fas fa-envelope-open-text contact__icon"></i>
+                >
+                    <i class="fas fa-envelope-open-text contact__icon"></i>
                 </a>
             </div>
         </section>
@@ -67,26 +72,21 @@
                     target="_blank"
                     rel="noreferrer"
                     class="project__link"
-                    title="kitty friends"
-                    >&rarr; ZeroGen Apparel</a
-                >
+                    title="ZeroGen Apparel"
+                >&rarr; ZeroGen Apparel</a>
                 <i class="fab fa-vuejs icon"></i>
             </div>
 
             <div class="projects__project">
                 <a
-                    href="https://face-recognition-brain-4293.herokuapp.com/"
+                    href="https://www.winning-streak.com"
                     target="_blank"
                     class="project__link"
                     rel="noreferrer"
-                    title="face-recognition brain"
-                >
-                    &rarr; Face-Recognition Brain
-                </a>
+                    title="Winning Streak"
+                >&rarr; Winning Streak</a>
                 <div class="project__icons">
-                    <i class="fas fa-database icon"></i>
-                    <i class="fab fa-node-js icon"></i>
-                    <i class="fab fa-react icon"></i>
+                    <i class="fab fa-vuejs icon"></i>
                 </div>
             </div>
 
@@ -97,8 +97,7 @@
                     class="project__link"
                     title="eSportacus"
                     rel="noreferrer"
-                    >&rarr; eSportacus
-                </a>
+                >&rarr; eSportacus</a>
                 <i class="fab fa-aws icon"></i>
                 <i class="fab fa-node-js icon"></i>
                 <i class="fab fa-sass icon"></i>
@@ -111,22 +110,24 @@
                 class="projects__github"
                 rel="noreferrer"
                 title="ronnie's github"
-                >more on github <i class="fab fa-github projects__github--icon"></i
-            ></a>
+            >
+                more on github
+                <i class="fab fa-github projects__github--icon"></i>
+            </a>
         </section>
     </div>
 </template>
 
 <script>
-import Name from '@/components/Name'
+import Name from '@/components/Name';
 
 export default {
     metaInfo: {
         title: 'Ronini.dev',
     },
     components: {
-        Name
-    }
+        Name,
+    },
 };
 </script>
 
