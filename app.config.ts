00b5321addc914410a0976a2dbbb69a108058b32
Ronnie Villarini <ronnie@ronini.dev>
2023-11-08 22:30:34 -0600

    adjust base size for icons

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   nuxtIcon: {
-    size: "2rem", // default <Icon> size applied
+    size: "24px", // default <Icon> size applied
     class: "icon", // default <Icon> class applied
   },
 });
