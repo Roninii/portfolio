acc8f93e9a37571100939899fd0672314f327ed6
Ronnie Villarini <ronnie@ronini.dev>
2023-11-08 22:30:44 -0600

    abstract theme toggle

---
+++ components/ThemeToggle.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="toggleTheme" class="theme-toggle" title="toggle dark theme">
+    <Icon name="fa6-regular:lightbulb" />
+  </button>
+</template>
+
+<script setup lang="ts">
+const { toggleTheme } = useTheme();
+</script>
+
+<style scoped>
+.theme-toggle {
+  border: none;
+  line-height: 1rem;
+  background: none;
+  color: var(--primary);
+  font-size: 1.25rem;
+  cursor: pointer;
+  padding: 1rem;
+}
+</style>
