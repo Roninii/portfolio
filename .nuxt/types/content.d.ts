3bf10a3727f8d0bfd7e11b49768ccffd527f6b2d
Ronnie Villarini <ronnie@ronini.dev>
2023-11-16 20:51:55 -0600

    respect .gitignore

--- .nuxt/types/content.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '#content/server' {
-  const serverQueryContent: typeof import('/home/ronini/dev/portfolio/node_modules/@nuxt/content/dist/runtime/legacy/types').serverQueryContent
-  const parseContent: typeof import('/home/ronini/dev/portfolio/node_modules/@nuxt/content/dist/runtime/server').parseContent
-}
